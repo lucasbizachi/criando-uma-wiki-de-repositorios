771552c833ca6799c91cb00cc31323e8aa70c278
lucasbizachi <bizachilucas@gmail.com>
2024-04-23 15:42:25 -0300

    Criando Input

--- desafio-wiki/src/pages/App.js
+++ desafio-wiki/src/pages/App.js
@@ -1,10 +1,12 @@
 import gitLogo from '../assets/github02.png';
+import Input from '../components/Input';
 import { Container } from './styles';
 
 function App() {
   return (
     <Container Container>
       <img src={gitLogo} width={72} height={72} alt="GitHub Logo" />
+      <Input />
     </Container>
   );
 }
--- desafio-wiki/src/pages/styles.js
+++ desafio-wiki/src/pages/styles.js
@@ -5,9 +5,10 @@ export const Container = styled.div`
     width: 100%;
     height: 90vh;
 
-    margin: 20px 0;
+    margin: 20px 0 0 0;
 
     display: flex;
-    align-itens: flex-start;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
 `
